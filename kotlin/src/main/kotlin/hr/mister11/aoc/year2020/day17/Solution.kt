0768cab372e2b2100e3ca6986a3e2bbde8750819
Sven Vidak <sven.vidak@gmail.com>
2021-11-23 01:47:53 +0100

    2020-day17 placeholder

---
+++ kotlin/src/main/kotlin/hr/mister11/aoc/year2020/day17/Solution.kt
@@ -0,0 +1,17 @@
+package hr.mister11.aoc.year2020.day17
+
+import hr.mister11.aoc.Resources
+
+fun main() {
+
+    val solution = Solution(Resources.readFileAsList(2020, 17))
+    println("Part 1: ${solution.part1()}")
+    println("Part 2: ${solution.part2()}")
+}
+
+class Solution(input: List<String>) {
+
+    fun part1() = 0
+
+    fun part2() = 0
+}
