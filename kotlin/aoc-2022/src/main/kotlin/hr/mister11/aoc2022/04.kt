0e0b90b7998d8b8fc83cc76604a651533a0fa0d3
Sven Vidak <sven.vidak@gmail.com>
2022-12-04 13:51:59 +0100

    feat: day 4

---
+++ kotlin/aoc-2022/src/main/kotlin/hr/mister11/aoc2022/04.kt
@@ -0,0 +1,64 @@
+package hr.mister11.aoc2022
+
+fun main() {
+    val input = readInput("04")
+    
+    println("Part 1: ${Solution04.part1(input)}")
+    println("Part 2: ${Solution04.part2(input)}")
+}
+
+
+object Solution04 {
+    fun part1(input: String): String {
+        return input
+            .lines()
+            .map { line -> line.split(",") }
+            .map { elfRanges ->
+               val range1 = parseRange(elfRanges[0])
+                val range2 = parseRange(elfRanges[1])
+                Pair(range1, range2)
+            }
+            .count { (range1, range2) ->
+                val range1Start = range1.first
+                val range1End = range1.last
+                val range2Start = range2.first
+                val range2End = range2.last
+
+                val firstInSeconds = range1Start in range2 && range1End in range2
+                val secondInFirst = range2Start in range1 && range2End in range1
+
+                firstInSeconds || secondInFirst
+            }
+            .toString()
+    }
+
+    fun part2(input: String): String {
+        return input
+            .lines()
+            .map { line -> line.split(",") }
+            .map { elfRanges ->
+                val range1 = parseRange(elfRanges[0])
+                val range2 = parseRange(elfRanges[1])
+                Pair(range1, range2)
+            }
+            .count { (range1, range2) ->
+                val range1Start = range1.first
+                val range1End = range1.last
+                val range2Start = range2.first
+                val range2End = range2.last
+
+                val firstInSeconds = range1Start in range2 || range1End in range2
+                val secondInFirst = range2Start in range1 || range2End in range1
+
+                firstInSeconds || secondInFirst
+            }
+            .toString()
+    }
+
+    private fun parseRange(range: String): IntRange {
+        val tokens = range.split("-")
+        val a = tokens[0].toInt()
+        val b = tokens[1].toInt()
+        return IntRange(a, b)
+    }
+}
---
+++ kotlin/aoc-2022/src/test/kotlin/hr/mister11/aoc2022/04.kt
@@ -0,0 +1,17 @@
+package hr.mister11.aoc2022
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Solution04Test {
+
+    @Test
+    fun testPart1() {
+        assertEquals("2", Solution04.part1(readExample("04")))
+    }
+
+    @Test
+    fun testPart2() {
+        assertEquals("4", Solution04.part2(readExample("04")))
+    }
+}
